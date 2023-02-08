4fe462ba22c9a53272ccfa14cddfa94cac0f398b
gsnorberto <sn_gabriel@outlook.com>
2023-02-08 12:42:07 -0300

    feat: Adiciona a função de obter clientes pelo id

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -13,29 +13,37 @@ export const getCustomers = async (req, res) => {
     }
 }
 export const getCustomerById = async (req, res) => {
-    try {
+    let { id } = req.params
 
+    try {
+        const customer = await db.query(`SELECT * FROM customers WHERE id = '${id}'`)
+        
+        if(customer.rows.length === 0) {
+            return res.sendStatus(404)
+        }
+
+        res.send(customer.rows);
     }
     catch (err) {
-
+        res.status(500).send(err.message)
     }
 }
 export const addCustomer = async (req, res) => {
+    const { name, phone, cpf, birthday } = req.body
+
+    //  check if the birthday value is a valid date
+    if (!dayjs(birthday, 'YYYY-MM-DD', true).isValid()) {
+        console.log("foi")
+        return res.sendStatus(400)
+    }
+
+    // check if the cpf already exists in the DB
+    const customer = await db.query(`SELECT * FROM customers WHERE cpf = '${cpf}'`)
+    if(customer.rows.length > 0){
+        return res.sendStatus(409)
+    }
+
     try {
-        const { name, phone, cpf, birthday } = req.body
-
-        //  check if the birthday value is a valid date
-        if (!dayjs(birthday, 'YYYY-MM-DD', true).isValid()) {
-            console.log("foi")
-            return res.sendStatus(400)
-        }
-
-        // check if the cpf already exists in the DB
-        const customer = await db.query(`SELECT * FROM customers WHERE cpf = '${cpf}'`)
-        if(customer.rows.length > 0){
-            return res.sendStatus(409)
-        }
-
         const query = `
             INSERT INTO
                 customers (name, phone, cpf, birthday)
