965d41b2899c91b307f406aa2d7502177f2025e8
gsnorberto <sn_gabriel@outlook.com>
2023-02-08 12:29:17 -0300

    feat: Adiciona a função de obter clientes cadastrados

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -5,7 +5,8 @@ dayjs.extend(customParseFormat)
 
 export const getCustomers = async (req, res) => {
     try {
-        
+        const customers = await db.query("SELECT * FROM customers")
+        res.send(customers.rows);
     }
     catch (err) {
         res.status(500).send(err.message)
