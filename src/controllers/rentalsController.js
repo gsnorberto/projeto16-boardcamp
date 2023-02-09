675f8cb732055a3cdf156dbb97b097117fbe73e7
gsnorberto <sn_gabriel@outlook.com>
2023-02-09 10:38:49 -0300

    feat: Adiciona a função de cadastro de aluguéis;

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -1,4 +1,5 @@
 import { db } from "../config/database.js"
+import dayjs from 'dayjs'
 
 export const getRentals = async (req, res) => {
     try{
@@ -10,8 +11,43 @@ export const getRentals = async (req, res) => {
 }
 
 export const addRental = async (req, res) => {
+    let { customerId, gameId, daysRented } = req.body
+
+    if (daysRented <= 0 ){
+        return res.sendStatus(400)
+    }
+
     try{
-        
+        const game = await db.query('SELECT * FROM games WHERE id = $1', [gameId])
+        if(game.rows.length === 0){ // game not found
+            return res.sendStatus(400)
+        }
+
+        const customer = await db.query('SELECT * FROM customers WHERE id = $1', [customerId])
+        if(customer.rows.length === 0){ // customer not found
+            return res.sendStatus(400)
+        }
+
+        const rentals = await db.query('SELECT * FROM rentals WHERE "gameId" = $1', [gameId])
+        if(rentals.rows.length === game.rows[0].stockTotal){ // game unavailable for rent
+            return res.sendStatus(400)
+        }
+
+        let rentDate = dayjs(Date.now()).format('YYYY-MM-DD')
+        console.log(rentDate)
+        let originalPrice = Number(daysRented) * Number(game.rows[0].pricePerDay)
+        let returnDate = null;
+        let delayFee = null;
+
+        let query = `
+            INSERT INTO 
+                rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")
+            VALUES 
+                ($1, $2, $3, $4, $5, $6, $7)
+        `
+
+        await db.query(query, [customerId, gameId, rentDate, daysRented, returnDate, originalPrice, delayFee])
+        res.sendStatus(201)
     } catch(err){
         res.status(500).send(err.message)
     }
